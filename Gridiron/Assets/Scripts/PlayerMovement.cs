8d20f4d4de6836d3f9e829530d12f788b769bb7c
Kristian Utvær <kristianu@uia.no>
2022-03-21 12:39:31 +0100

    player movment

--- Gridiron/Assets/Scripts/PlayerMovement.cs
+++ Gridiron/Assets/Scripts/PlayerMovement.cs
@@ -1,18 +1,31 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    private PlayerInput _playerInput;
+    private float _moveSpeed = 1;
+
+
+    private void Start()
     {
-        
+        _playerInput = GetComponent<PlayerInput>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        Movement();
     }
+
+    
+
+    private void Movement()
+    {
+        transform.position +=
+            new Vector3(_playerInput.horizontal * _moveSpeed * Time.deltaTime, 0f, _playerInput.vertical * _moveSpeed * Time.deltaTime) ;
+    }
+    
 }
